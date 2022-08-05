0c08edd2bc799e35c2da7ff57e91fe4ceeeb60f7
Zheng Lu <zhenglu@Zheng-Lus-MacBook-Pro.local>
2022-08-05 10:35:56 +0800

    add day 1 and 2

---
+++ src/test/java/Day1Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class Day1Test {
+
+    @Test
+    public void Case1() {
+        System.out.println("day 1 test case 1");
+    }
+
+    @Test
+    public void Case2() {
+        System.out.println("day 1 test case 2");
+        // test test
+    }
+}
---
+++ src/test/java/Day2Test.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+public class Day2Test {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("before class");
+    }
+
+    @Before
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("after class");
+    }
+
+    @Test
+    public void Case1() {
+        System.out.println("test case 1");
+    }
+
+    @Test
+    public void Case2() {
+        System.out.println("test case 2");
+    }
+}
